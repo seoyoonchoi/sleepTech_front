ed788c810d378680a91a3624ad8155eb772bde5d
seoyoonchoi <chsu0904@naver.com>
2025-03-13 16:09:07 +0900

    javascript-start

---
+++ d_JavaScript/d_제어문/a_condition01.js
@@ -0,0 +1,16 @@
+  let age=12;
+  if(age<13) console.log('어린이');
+  else if(age<20) console.log('청소년');
+  else console.log('성인');
+
+  let score=73;
+  let grade;
+
+  if(score>100||score<0) console.log('invalid');
+  else if(score>=90) console.log('A'); 
+  else if(score>=80) console.log('B'); 
+  else if(score>=70) console.log('C'); 
+  else if(score>=60) console.log('D'); 
+  else console.log('F'); 
+  
+  true || console.log('출력');
---
+++ d_JavaScript/d_제어문/b_loop.js
@@ -0,0 +1,5 @@
+let fruits = ['apple', 'banana','mango'];
+let numbers = [1,2,3,4,5];
+let bools = [true,false];
+
+//js 배열에는 요소의 모든 타입이 동시에 삽입 가능하다.
