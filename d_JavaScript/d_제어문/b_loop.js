b1143dba8032bd02ecc42e63c68514ed1eb1f3e0
seoyoonchoi <chsu0904@naver.com>
2025-03-13 16:18:59 +0900

    반복문

--- d_JavaScript/d_제어문/b_loop.js
+++ d_JavaScript/d_제어문/b_loop.js
@@ -2,4 +2,19 @@ let fruits = ['apple', 'banana','mango'];
 let numbers = [1,2,3,4,5];
 let bools = [true,false];
 
-//js 배열에는 요소의 모든 타입이 동시에 삽입 가능하다.
+
+//js 배열에는 요소의 모든 타입이 동시에 삽입
+
+for(let i = 1; i<6;i++){
+  let stars = '';
+  for(let j=0;j<i;j++){
+    stars+='*';
+  }
+  console.log(stars);
+}
+
+let b = 0;
+while(b<5){
+  console.log(b);
+  b++;
+}
