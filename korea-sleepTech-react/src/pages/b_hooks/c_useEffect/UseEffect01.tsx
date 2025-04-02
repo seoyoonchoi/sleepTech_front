fb6fae3acd2e4fa0588c81a799d3eb0219a3a09b
seoyoonchoi <chsu0904@naver.com>
2025-04-02 09:32:15 +0900

    0401React

---
+++ korea-sleepTech-react/src/pages/b_hooks/c_useEffect/UseEffect01.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UseEffect01() {
+  return (
+    <div>UseEffect01</div>
+  )
+}
+
+export default UseEffect01
--- korea-sleepTech-react/src/pages/b_hooks/index.tsx
+++ korea-sleepTech-react/src/pages/b_hooks/index.tsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import UseState01 from './a_useState/useState01';
-import UseState02 from './a_useState/UseState02';
-import UseState03 from './a_useState/UseState03';
+import React from "react";
+
+import UseState01 from "./a_useState/UseState01";
+import UseState02 from "./a_useState/UseState02";
+import UseState03 from "./a_useState/UseState03";
+import UseState04 from "./a_useState/UseState04";
+import UseState05 from "./a_useState/UseState05";
+import UseState06 from "./a_useState/UseState06";
+import UseStatePractice from "./a_useState/Practice";
+
+import UseRef01 from "./b_useRef/UseRef01";
+import UseRef02 from "./b_useRef/UseRef02";
+import UseRefPractice from "./b_useRef/Practice";
+
+const h2Style = {
+  backgroundColor: "black",
+  color: "orange",
+};
 
 function Index() {
   return (
     <div>
-      <h1 style={{
-        backgroundColor: "black",
-        color: "pink",
-      }}> ===리액트=====
-
+      <h1
+        style={{
+          backgroundColor: "black",
+          color: "pink",
+        }}
+      >
+        === 리액트 Hooks ===
       </h1>
-      <h2 style = {{backgroundColor: "black",
-        color: "orange",}}>리액트</h2>
-      <UseState01/>
-      <hr />
-      <UseState02/>
-      <hr />
-      <UseState03/>
+
+      <h2 style={h2Style}>리액트 Hooks - useRef</h2>
+      <UseRef01 />
+      <UseRef02 />
+      <UseRefPractice />
+
+      <h2 style={h2Style}>리액트 Hooks - useState</h2>
+      <UseState01 /> <hr />
+      <UseState02 /> <hr />
+      <UseState03 /> <hr />
+      <UseState04 /> <hr />
+      <UseState05 /> <hr />
+      <UseState06 /> <hr />
+      <UseStatePractice />
 
     </div>
-  )
+  );
 }
 
 export default Index;
